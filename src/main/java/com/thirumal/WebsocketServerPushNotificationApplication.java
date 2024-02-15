001d2be3ada59b627dc25b174988f7850a6da992
Thirumal <m.thirumal@hotmail.com>
2024-02-15 09:42:08 +0530

    first commit

---
+++ src/main/java/com/thirumal/WebsocketServerPushNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketServerPushNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketServerPushNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thirumal/WebsocketServerPushNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirumal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketServerPushNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
